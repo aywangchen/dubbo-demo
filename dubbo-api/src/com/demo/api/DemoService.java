f7b1da650a2eec66da16100efaa3c0de7f8d68ab
aywangchen <aywangchen@126.com>
2017-03-29 14:51:44 +0800

    添加api结构

---
+++ dubbo-api/src/com/demo/api/DemoService.java
@@ -0,0 +1,8 @@
+package com.demo.api;
+
+/**
+ * Created by wangc on 2017/3/29.
+ */
+public interface DemoService {
+    public void testService();
+}
---
+++ dubbo-provider/src/main/java/com/dubbo/demo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.dubbo.demo.service.impl;
+
+import com.demo.api.DemoService;
+
+/**
+ * Created by wangc on 2017/3/29.
+ */
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public void testService() {
+
+    }
+}
